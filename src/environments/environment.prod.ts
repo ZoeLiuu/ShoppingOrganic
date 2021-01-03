b801dc860017dea8ff8ee3e972add8a254745b18
Loyu Liu <3oe.liu@gmail.com>
2021-01-03 23:33:15 +0800

    initial project setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCvDE-Ovy-60S48ghs_2eaNoFqX6At6hSE",
+    authDomain: "oshop-fe22b.firebaseapp.com",
+    projectId: "oshop-fe22b",
+    storageBucket: "oshop-fe22b.appspot.com",
+    messagingSenderId: "22949594998",
+    appId: "1:22949594998:web:41698dedd43a0453c71c57",
+    measurementId: "G-2T06B9630W"
+  }
 };
