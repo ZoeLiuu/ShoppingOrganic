dd999ada512dbf26c63ef35b1e05560b7abd6db5
Loyu Liu <3oe.liu@gmail.com>
2021-01-04 01:14:14 +0800

    Login/Logout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  ngOnInit(): void {
+  login() {
+    this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
-
 }
