11a982804e4da689563baf9e2a7861483599f3b0
Loyu Liu <3oe.liu@gmail.com>
2021-01-19 01:42:03 +0800

    render product list

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/product.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-products',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor() { }
+  products$;
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll()
+      .snapshotChanges()
+      .pipe(map(changes => changes
+        .map(c => ({ key: c.payload.key, ...c.payload.val() }))))
+  }
 
   ngOnInit(): void {
   }
