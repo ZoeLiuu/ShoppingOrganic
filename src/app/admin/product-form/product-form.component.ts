464d3635d3480c535bd57defeec399f434bbda14
Loyu Liu <3oe.liu@gmail.com>
2021-01-19 01:24:22 +0800

    add bootstrap card

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
 
@@ -11,6 +12,7 @@ export class ProductFormComponent implements OnInit {
   categories$;
 
   constructor(
+    private router: Router,
     private categoryService: CategoryService,
     private productService: ProductService) {
     this.categories$ = this.categoryService.getCategories();
@@ -18,7 +20,7 @@ export class ProductFormComponent implements OnInit {
 
   save(product) {
     this.productService.create(product);
-
+    this.router.navigate(['/admin/products']);
   }
 
   ngOnInit(): void {
